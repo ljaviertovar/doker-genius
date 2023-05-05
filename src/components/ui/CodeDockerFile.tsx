6afc00cfad4713c1212d9f48e8bcc8a0ca9c9da8
ljaviertovar <luisjavier_tovar@outlook.com>
2023-05-05 00:16:05 -0400

    add dockerfile components

---
+++ src/components/ui/CodeDockerFile.tsx
@@ -0,0 +1,37 @@
+import dynamic from "next/dynamic"
+const SyntaxHighlighter = dynamic(() => import("react-syntax-highlighter"))
+import { gradientDark } from "react-syntax-highlighter/dist/cjs/styles/hljs"
+
+interface Props {
+	textCode: string
+}
+
+export default function CodeDockerFile({ textCode }: Props) {
+	return (
+		<div>
+			<SyntaxHighlighter
+				language='bash'
+				// style={isThemeDark ? dracula : vs}
+				style={gradientDark}
+				wrapLines={true}
+				showLineNumbers={false}
+				// lineNumberStyle={{ color: isThemeDark ? "gray" : "#ccc" }}
+				lineNumberStyle={{ color: "#ccc" }}
+				customStyle={{
+					maxHeight: "none",
+					height: "auto",
+					overflow: "visible",
+					wordWrap: "break-word",
+					// color: "inherit",
+					// backgroundColor: isThemeDark ? "#1D1D1D" : "#F8F8F8",
+					// backgroundColor: "#1D1D1D",
+					borderRadius: "0.5rem",
+					padding: "1rem",
+				}}
+				lineProps={{ style: { whiteSpace: "pre-wrap" } }}
+			>
+				{textCode}
+			</SyntaxHighlighter>
+		</div>
+	)
+}
---
+++ src/data/placeholders.ts
@@ -0,0 +1,2 @@
+export const dockerfilePlaceholder =
+	'# Use an official Node.js runtime as a parent image\nFROM node:14.16.0-alpine3.13\n\n# Set the working directory to /app\nWORKDIR /app\n\n# Copy the package.json and package-lock.json to the container\nCOPY package*.json ./\n\n# Install dependencies\nRUN npm install --production --silent\n\n# Copy the rest of the application code to the container\nCOPY . .\n\n# Expose port 3000 for the container\nEXPOSE 3000\n\n# Start the React app when the container starts\nCMD ["npm", "start"]'

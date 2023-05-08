3f65ea6e17c7d4d742bea221a4188367940a78af
ljaviertovar <luisjavier_tovar@outlook.com>
2023-05-07 23:30:53 -0400

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
-import "@/styles/globals.css"
-import { ThemeProvider } from "next-themes"
-
 import type { AppProps } from "next/app"
+import { ThemeProvider } from "next-themes"
+import { Roboto } from "@next/font/google"
+
+const ubuntu = Roboto({
+	subsets: ["latin"],
+	weight: ["300", "400", "700"],
+})
+
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider enableColorScheme attribute='class'>
-			<Component {...pageProps} />
+			<div className={ubuntu.className}>
+				<Component {...pageProps} />
+			</div>
 		</ThemeProvider>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import MainLayout from "@/components/layouts/MainLayout"
 import useDockerfileGenerator from "../hooks/useDockerfileGenerator"
 import { dockerfilePlaceholder, inputPlaceholder } from "@/data/placeholders"
 import CodeDockerFile from "@/components/ui/CodeDockerFile"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCopy } from "@fortawesome/free-solid-svg-icons"
 import GenerateButton from "@/components/ui/GenerateButton"
+import CopyButton from "@/components/ui/CopyButton"
+import CreateButton from "../components/ui/CreateButton"
 
 export default function HomePage() {
 	const { generate, generating, error, dockerfile } = useDockerfileGenerator()
@@ -21,43 +21,36 @@ export default function HomePage() {
 	return (
 		<MainLayout title={"Doker Genius"} pageDescription={"Dockerfiles Generator"}>
 			<div className='w-full mb-12 text-center'>
-				<h1 className='text-4xl font-bold mb-2'>Doker Genius</h1>
-				<h2 className='text-2xl font-semibold text-custom-dark-50'>Dockerfile validator and generator</h2>
+				<h1 className='text-4xl font-bold mb-2 font-mono'>DokerGenius</h1>
+				<h2 className='text-2xl font-semibold text-custom-dark-50 '>Dockerfile validator and generator</h2>
 			</div>
 			<div className='flex gap-6 w-full'>
 				<div className='w-2/6 dark:bg-custom-dark-900 p-4 rounded-lg border border-custom-dark-800'>
-					<form onSubmit={handleSubmit}>
-						<label className='text-lg text-custom-dark-50'>Enter the instructions for your Dockerfile:</label>
+					<form onSubmit={handleSubmit} className='mb-8 text-center'>
+						<label className='text-md text-left text-custom-dark-50 block mb-2'>
+							Enter the instructions for your Dockerfile:
+						</label>
 						<textarea
-							className={`border border-custom-dark-800 appearance-none rounded-lg w-full p-4 bg-custom-gray-bg dark:bg-custom-dark-950 text-gray-700 dark:text-custom-dark-50 leading-tight mb-4 mt-2`}
+							className={` text-md border border-custom-dark-800 appearance-none rounded-lg w-full p-4 bg-custom-gray-bg dark:bg-custom-dark-950 text-gray-700 dark:text-custom-dark-50 leading-tight mb-4 mt-2`}
 							rows={6}
 							placeholder={inputPlaceholder}
 							name='prompt'
-							// onChange={ev => setPrompt(ev.target.value)}
-							// onKeyDown={ev => {
-							// 	if (ev.key === "Enter") {
-							// 		console.log(ev.target)
-							// 		handleSubmit(ev)
-							// 	}
-							// }}
 							required
 						/>
 
 						{error && <div>Error</div>}
 
-						<GenerateButton generating={generating} />
+						<div className='flex justify-between items-center'>
+							<CreateButton />
+							<span className='text-sm'>Or</span>
+							<GenerateButton generating={generating} />
+						</div>
 					</form>
 				</div>
 
 				<div className='w-4/6 dark:bg-custom-dark-900 p-4 rounded-lg border border-custom-dark-800 relative'>
 					<div className='absolute right-8 top-8'>
-						<button
-							type='button'
-							title='Copy'
-							className='dark:bg-custom-dark-950 border border-custom-dark-50 hover:border-white text-custom-dark-50 hover:text-white font-semibold py-2 px-4 rounded-lg'
-						>
-							<FontAwesomeIcon icon={faCopy} />
-						</button>
+						<CopyButton />
 					</div>
 					<CodeDockerFile textCode={(dockerfile as string) ?? dockerfilePlaceholder} />
 				</div>

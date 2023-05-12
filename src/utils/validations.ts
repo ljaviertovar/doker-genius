fea2bd2f6e3a6b68dbf1d9ef04e7a990665ab2e4
ljaviertovar <luisjavier_tovar@outlook.com>
2023-05-12 12:35:39 -0400

    update validations

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,11 +1,54 @@
+const KEYWORDS = [
+	"write",
+	"validate",
+	"create",
+	"generate",
+	"docker",
+	"dockerfile",
+	"file",
+	"script",
+	"system",
+	"db",
+	"database",
+	"api",
+	"rest",
+	"container",
+	"containers",
+	"image",
+	"dockerize",
+	"application",
+	"app",
+	"project",
+	"write a dockerfile",
+	"create a dockerfile",
+	"generate a dockerfile",
+	"escribe",
+	"valida",
+	"crea",
+	"genera",
+	"archivo",
+	"sistema",
+	"base de datos",
+	"contenedor",
+	"contenedores",
+	"imagen",
+	"applicacion",
+	"proyecto",
+	"escribe un dockerfile",
+	"crea un dockerfile",
+	"genera un dockerfile",
+]
+
 export const validateInputPrompt = (prompt: string): boolean => {
-	const regex =
-		/(write|validate|create|generate|application|docker|dockerfile|dockerize|app|application|write a dockerfile|create a dockerfile|generate a dockerfile|generate dockerfile|create dockerfile|write dockerfile|escribe|valida|crea|genera|aplicacion|escribe un dockerfile|crea un dockerfile|genera un dockerfile|escribe dockerfile|crea dockerfile|genera dockerfile)/gi
+	const words = prompt.match(/\w+/g)?.map(p => p.toLocaleLowerCase())
+	if (words?.length) {
+		const uniqueWords = [...new Set(words)]
+		const count = uniqueWords.filter(word => KEYWORDS.includes(word)).length
 
-	const matches = prompt.match(regex)
-	console.log(matches)
+		const regex = /(write|validate|create|generate|escribe|valida|crea|genera)\s+(un|a\s+)?dockerfile/i
 
-	if (matches && matches.length >= 3) return true
-
+		if (count >= 3 || regex.test(prompt)) return true
+		return false
+	}
 	return false
 }

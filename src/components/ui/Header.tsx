8ca227f5e7b2f542348c75659a922625441e3cef
ljaviertovar <luisjavier_tovar@outlook.com>
2023-05-12 10:50:49 -0400

    update Header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import RRSS from "../RRSS"
+import DockerGeniusIcon from "@/assets/icons/DockerGeniusIcon"
+
 // import ThemeButton from "./ThemeButton"
 
 export default function Header() {
@@ -9,8 +11,10 @@ export default function Header() {
 
 			<RRSS />
 			<div className='mt-4'>
-				<h1 className='text-5xl font-bold mb-0 font-mono text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-violet-600'>
-					DokerGenius
+				<h1 className='text-5xl font-bold mb-0 font-mono text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-violet-600 flex items-center w-full justify-center'>
+					Doker
+					<DockerGeniusIcon fill='#2563eb' width='2rem' />
+					Genius
 				</h1>
 				<h2 className='text-2xl  text-custom-dark-50 mb-0'>Dockerfile validator and generator</h2>
 			</div>
